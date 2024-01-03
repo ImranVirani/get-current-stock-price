55c9f3384a79f4f40b4b0deba3604b38c334d625
Imran Virani <virani.imran@icloud.com>
2024-01-03 17:27:35 -0500

    Created app.py and added call to stock data API

---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+def get_keys(path):
+    with open(path) as f:
+        return json.load(f)
+keys = get_keys("./apikey.json")
+API_Key = keys['api_key']
+
+
+def getStockPrice(stock, api_key):
+    price = requests.get(f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock}&apikey={api_key}').json()
+    price = ((price['Global Quote']['05. price']))
+    print(f'The price of ${stock} is {price}')
+
+stock_symbol = str(input("What stock symbol would you like the price of: "))
+getStockPrice(stock_symbol, api_key)
